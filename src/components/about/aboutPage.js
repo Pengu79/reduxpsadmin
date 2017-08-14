74e4fe34df699beca018f0121af632ffd385aaa0
Lindberg <jesper.lindberg@globaldhs.net>
2017-08-14 09:24:32 +0200

    First upload

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class aboutPage extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This app uses React, Redux, React Router and other stuff</p>
+            </div>
+        );
+    }
+}
+export default aboutPage;
